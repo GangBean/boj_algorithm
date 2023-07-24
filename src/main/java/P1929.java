36670d11afccf931b00c10b23ee5d07c28c31c87
jsh <xbox2537@gmail.com>
2023-07-24 11:19:21 +0900

    1929

--- src/main/java/P1929.java
+++ src/main/java/P1929.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class P1929 {
 
@@ -23,6 +25,21 @@ public class P1929 {
     */
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String s = reader.readLine();
+        int[] nums = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        boolean[] isPrime = new boolean[nums[1] + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[1] = false;
+        for (int i = 2; i * i <= nums[1]; i++) {
+            if (isPrime[i]) {
+                for (int j = 2; j * i <= nums[1]; j++) {
+                    isPrime[j * i] = false;
+                }
+            }
+        }
+        for (int i = nums[0]; i <= nums[1]; i++) {
+            if (isPrime[i]) {
+                System.out.println(i);
+            }
+        }
     }
 }
